c32af4cf1d3b843cc62d2e37970368bf4ca63d28
Axel <lezinaxel@gmail.com>
2016-11-30 10:03:06 +0100

    Les infos s'affichent correctement à l'apparition de la carte de profil

--- app.js
+++ app.js
@@ -9,16 +9,26 @@ console.log('start');
 			$('#profil').on('click', app.viewProfil);
 		},
 		viewProfil : function(){
-			var infos = {status : 'filleule', prenom : 'Axelle', nom : 'Lezina', job : 'Dev chez Blizzard'};
+			var infos = {
+				status : 'Filleule',
+				prenom : 'Axelle',
+				nom : 'Lezina',
+				job : 'Développeuse chez Blizzard Entertainement',
+				location : 'Toulouse',
+				description : 'Je suis très intelligente',
+				communication : 'Slack'
+			}
 			$('#carteProfil').show();
-			$('#carteProfil').text(infos.status + " " + infos.prenom +" "+ infos.nom +" "+ infos.job);
-			$('#carteProfil').css("background-color", 'white');
-			
-			
-
+			$('.status').text(infos.status);
+			$('.prenom').text(infos.prenom);
+			$('.nom').text(infos.nom);
+			$('.boulot').text(infos.job);
+			$('.lieu').text(infos.location);
+			$('.presentation').text(infos.description);
+			$('.communication').text(infos.communication);
+			$('#carteProfil').css("background-color", '#f100e5'); 
 		}
 	}
-
 
 
 
